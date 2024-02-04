89b0cb7008f633e6fc547e6ea351d4fdf2809d48
Victor <govs79@outlook.com>
2024-02-04 23:39:50 +0300

    Zadacha 3

---
+++ Zadacha3.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+int number = 55;
+int firstDigit = number/10;
+int secondDigit = number%10;
+int max = 0;
+//if (firstDigit = secondDigit)
+//{
+ //   Console.WriteLine("Числа равны");
+//}
+if (firstDigit>secondDigit)
+{
+    max = firstDigit;
+}
+else
+{
+    max = secondDigit;
+}
+Console.WriteLine($"Наибольшая цифра числа {number} - {max}");
